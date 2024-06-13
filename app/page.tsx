8b42e9b92e5630bb3d38dcab205fd4da1d8ec266
rakeshkanna-rk <rakeshkanna0108@gmail.com>
2024-06-13 13:42:37 +0530

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 
-import Navbar from "./NavBar";
+
+import Navbar from "./NavbarDemo";
 
 export default function Home() {
   return (
     <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2" />
+      <NavbarDemo className="top-2" />
       <p className="text-black dark:text-white">
         The Navbar will show on top of the page
       </p>
